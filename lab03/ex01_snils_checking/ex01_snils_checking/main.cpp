a4f70f7b656b59e9efcb4cc01a3d9e43df896ec8
T1vz <tima.bevz@gmail.com>
2022-10-22 12:48:07 +0300

    lab03 ex01 initial

---
+++ lab03/ex01_snils_checking/ex01_snils_checking/main.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const int ASCII_DIGITS_INDENT = 48;
+
+int printResult(bool flag) {
+	if (flag) {
+		cout << "True";
+	}
+	else {
+		cout << "False";
+	}
+	return 0;
+}
+
+bool isDigits(string s) {
+	return s.find_first_not_of("0123456789") == string::npos;
+}
+
+bool validate(string snils) {
+	if (snils.length() != 11 || !isDigits(snils)) { // snils must contain 11 chars and only digits
+		return false;
+	}
+
+	char lastChar = snils[0];
+	int sum = (int)lastChar - ASCII_DIGITS_INDENT; // in ASCII code the numbers(digits) start from 48
+	int charsInRow = 1;
+	for (int i = 1; i < 9; i++) // check every char of string
+	{
+		if (charsInRow == 3) { // snils mustn't contain 3 similar chars in a row
+			return false;
+		}
+
+		sum += ((int)snils[i] - ASCII_DIGITS_INDENT) * (i + 1); // in ASCII code the numbers(digits) start from 48 and we are doing condition #1
+
+		if (lastChar == snils[i]) { // counting chars in a row
+			charsInRow += 1;
+		}
+		else { // reseting counter
+			charsInRow = 1;
+			lastChar = snils[i];
+		}
+	}
+
+	int control = stoi(snils.substr(9, 11)); // control summ from entered string to int
+	if ((sum < 100 && control == sum) || ((sum == 100 || sum == 101) && control == 0)) {
+		return true;
+	}
+
+	if (sum > 101) {
+		int ost = sum % 101;
+		if ((ost < 100 && control == ost) || (ost == 100 && control == 0)) {
+			return true;
+		}
+	}
+	return false;
+}
+
+
+string getStringWithoutSpeials(string s) {
+	s.erase(remove(s.begin(), s.end(), ' '), s.end());
+	s.erase(remove(s.begin(), s.end(), '-'), s.end());
+	return s;
+}
+
+int main()
+{
+	cout << "Enter your SNILS: ";
+	string snils;
+	getline(cin, snils);
+
+	// remove whitespaces and defise
+	snils = getStringWithoutSpeials(snils);
+
+	return printResult(validate(snils));
+}
