a08ecd195a7d987f62cee7cbbc41c2150a7f8d9d
Timofey Bevz <71010887+T1vz@users.noreply.github.com>
2022-12-10 17:04:07 +0300

    bug fix

--- lab13/ex01_polymorphism/ex01_polymorphism/student.h
+++ lab13/ex01_polymorphism/ex01_polymorphism/student.h
@@ -3,21 +3,16 @@
 #include <vector>
 class student : public human {
 public:
-	// Конструктор класса Student
 	student(std::string last_name, std::string name, std::string
 		second_name,
 		std::vector<int> scores) : human(last_name, name,
 			second_name) {
 		this->scores = scores;
 	}
-	// Получение среднего балла студента
 	float get_average_score()
 	{
-		// Общее количество оценок
 		unsigned int count_scores = this->scores.size();
-		// Сумма всех оценок студента
 		unsigned int sum_scores = 0;
-		// Средний балл
 		float average_score;
 		for (unsigned int i = 0; i < count_scores; ++i) {
 			sum_scores += this->scores[i];
@@ -30,6 +25,5 @@ public:
 		std::cout << "Status: Student;" << "Name: " << this->get_full_name() << "; Avarage score: " << this->get_average_score() << "\n";
 	}
 private:
-	// Оценки студента
 	std::vector<int> scores;
 };
