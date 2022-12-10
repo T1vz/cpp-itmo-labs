6926a529c14b2a5ba90f12168654e3c1b6fc4cba
Timofey Bevz <71010887+T1vz@users.noreply.github.com>
2022-12-10 17:04:52 +0300

    bug fix

--- lab13/ex01_polymorphism/ex01_polymorphism/teacher.h
+++ lab13/ex01_polymorphism/ex01_polymorphism/teacher.h
@@ -2,13 +2,11 @@
 #include "human.h"
 #include <string>
 class teacher : public human {
-	// Конструктор класса teacher
 public:
 	teacher(
 		std::string last_name,
 		std::string name,
 		std::string second_name,
-		// Количество учебных часов за семестр у преподавателя
 		unsigned int work_time
 	) : human(
 		last_name,
@@ -17,7 +15,6 @@ public:
 	) {
 		this->work_time = work_time;
 	}
-	// Получение количества учебных часов
 	unsigned int get_work_time()
 	{
 		return this->work_time;
@@ -28,6 +25,5 @@ public:
 		std::cout << "Status: Teacher;" << "Name: " << this->get_full_name() << "; Working time: " << this->get_work_time() << "\n";
 	}
 private:
-	// Учебные часы
 	unsigned int work_time;
 };
